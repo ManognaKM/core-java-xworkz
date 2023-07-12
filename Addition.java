1b6d48a911072e82a2fc0fddd6171aec583352a5
ManognaKM <manognakm250@gmail.com>
2023-07-12 14:17:49 +0530

    addition java program

---
+++ Addition.java
@@ -0,0 +1,19 @@
+class Addition{
+	
+	public static void main(String aggs[]){
+		add(56,78,5);
+		add(112,255,55);
+		add(1,5,2);
+		add(6,8,5);
+		add(4,3,2);
+		add(1,1,2);
+		add(7,7,7);
+		add(3,4,8);
+		add(1,4,3);
+		add(99,88,555);
+		
+	}
+	public static void add(int z, int u, int x){
+		System.out.println(z+u+x);
+	}
+}
